bb4f28ed9d292edc26d2309495d2720be30f0452
Scott <scottthomas3000@gmail.com>
2015-08-18 21:23:45 -0400

    Added name validator

--- app/src/main/java/com/example/scott/rapitap/NameInputActivity.java
+++ app/src/main/java/com/example/scott/rapitap/NameInputActivity.java
@@ -5,13 +5,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class NameInputActivity extends Activity{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NameInputActivity extends Activity {
 
     EditText playerNameInput;
     TextView enterBtnTextView;
@@ -25,23 +29,46 @@ public class NameInputActivity extends Activity{
         // Set up calls
         playerNameInput = (EditText) findViewById(R.id.playerNameInput);
         enterBtnTextView = (TextView) findViewById(R.id.enterBtnTextView);
+        enterBtnTextView.setHintTextColor(Color.BLACK);
+        enterBtnTextView.setHint("Type Initials up to 3 characters");
 
         // Set up button
         enterBtnTextView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                playerName = playerNameInput.getText().toString();
+                final String name = playerNameInput.getText().toString();
 
-                SharedPreferences scorePref = getSharedPreferences("userScore", Context.MODE_PRIVATE);
-                SharedPreferences.Editor scoreEditor = scorePref.edit();
-                String newUserName = scorePref.getString("newUserName", "");
-                scoreEditor.putString("newUserName", String.valueOf(playerName));
-                scoreEditor.apply();
+                if (isValidName(name)) {
 
-                Intent levelOneIntent = new Intent(NameInputActivity.this, LevelOneActivity.class);
-                finish();
-                startActivity(levelOneIntent);
+                    playerName = playerNameInput.getText().toString();
+
+                    SharedPreferences scorePref = getSharedPreferences("userScore", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor scoreEditor = scorePref.edit();
+                    String newUserName = scorePref.getString("newUserName", "");
+                    scoreEditor.putString("newUserName", String.valueOf(playerName));
+                    scoreEditor.apply();
+
+                    Intent levelOneIntent = new Intent(NameInputActivity.this, LevelOneActivity.class);
+                    finish();
+                    startActivity(levelOneIntent);
+                }
+
+                if (!isValidName(name)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Initials should be 3 characters in length", Toast.LENGTH_LONG)
+                            .show();
+                    finish();
+                    startActivity(getIntent());
+                }
             }
         });
+    }
+
+    private boolean isValidName(String name) {
+        String NAME = "^[a-z0-9_-]{3,3}$";
+
+        Pattern pattern = Pattern.compile(NAME, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
     }
 }
